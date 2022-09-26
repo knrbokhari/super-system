935d0ac59191721a47ed00ec3bddb2ac60d495d1
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-09-26 21:29:27 +0600

    adding react router dom and creating all pages

--- super-system/src/App.js
+++ super-system/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import Home from "./pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import List from "./pages/List/List";
+import Single from "./pages/Single/Single";
+import New from "./pages/New/New";
 
 function App() {
-  return <div>super system</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="list" element={<List />} />
+        <Route path="new" element={<New />} />
+        <Route path="single" element={<Single />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
