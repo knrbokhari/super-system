2404f5a4eaa413b79a716ab77a99911268f1d048
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-09-27 02:04:08 +0600

    creating featured component

---
+++ super-system/src/components/Chart/Chart.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Chart.scss";
+
+const Chart = () => {
+  return <div className="chart">Chart</div>;
+};
+
+export default Chart;
--- super-system/src/pages/Home/Home.js
+++ super-system/src/pages/Home/Home.js
@@ -3,6 +3,8 @@ import "./Home.scss";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Navbar from "../../components/Navbar/Navbar";
 import Widget from "../../components/Widget/Widget";
+import Featured from "../../components/Featured/Featured";
+import Chart from "../../components/Chart/Chart";
 
 const Home = () => {
   return (
@@ -14,6 +16,10 @@ const Home = () => {
           <Widget type="user" />
           <Widget type="order" />
           <Widget type="earning" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart />
         </div>
       </div>
     </div>
