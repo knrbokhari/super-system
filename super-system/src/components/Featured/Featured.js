f74db71b4675670454d941c1eeed870de106772c
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-09-27 02:21:09 +0600

    creating chart component

--- super-system/src/components/Featured/Featured.js
+++ super-system/src/components/Featured/Featured.js
@@ -22,7 +22,7 @@ const Featured = () => {
         <p className="desc">
           Previous transactions processing. Last payments may not be included.
         </p>
-        <div className="summary">
+        {/* <div className="summary">
           <div className="item">
             <div className="itemTitle">Target</div>
             <div className="itemResult negative">
@@ -44,7 +44,7 @@ const Featured = () => {
               <div className="resultAmount">$12.4k</div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
