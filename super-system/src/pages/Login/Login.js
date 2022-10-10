876324b1328d6cc6656157bde62bcc7ece83812b
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-10-10 20:19:39 +0600

    login page created

--- super-system/src/pages/Login/Login.js
+++ super-system/src/pages/Login/Login.js
@@ -1,7 +1,65 @@
 import React from "react";
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    navigate("/");
+  };
+  return (
+    <div>
+      <Box
+        component="main"
+        sx={{
+          alignItems: "center",
+          display: "flex",
+          flexGrow: 1,
+          minHeight: "100%",
+        }}
+      >
+        <Container maxWidth="sm">
+          <form>
+            <Box sx={{ my: 3 }}>
+              <Typography color="textPrimary" variant="h4">
+                Sign in
+              </Typography>
+            </Box>
+            <TextField
+              fullWidth
+              label="Email Address"
+              margin="normal"
+              name="email"
+              type="email"
+              variant="outlined"
+              value="admin@gmail.com"
+            />
+            <TextField
+              fullWidth
+              label="Password"
+              margin="normal"
+              name="password"
+              type="password"
+              variant="outlined"
+              value="123456"
+            />
+            <Box sx={{ py: 2 }}>
+              <Button
+                color="primary"
+                fullWidth
+                size="large"
+                type="submit"
+                variant="contained"
+                onClick={handleLogin}
+              >
+                Sign In
+              </Button>
+            </Box>
+          </form>
+        </Container>
+      </Box>
+    </div>
+  );
 };
 
 export default Login;
