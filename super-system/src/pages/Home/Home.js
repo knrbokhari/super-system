865a185ae57c4e7d31d45793a7f426406675cba3
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-10-13 20:52:54 +0600

    Home Page updated

--- super-system/src/pages/Home/Home.js
+++ super-system/src/pages/Home/Home.js
@@ -1,34 +1,47 @@
 import React from "react";
-import "./Home.scss";
-import Sidebar from "../../components/Sidebar/Sidebar";
-import Navbar from "../../components/Navbar/Navbar";
 import Widget from "../../components/Widget/Widget";
 import Featured from "../../components/Featured/Featured";
 import Chart from "../../components/Chart/Chart";
 import TableList from "../../components/Table/TableList";
+import { Box, Grid, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const Typographys = styled(Typography)(({ theme }) => ({
+  fontSize: "30px",
+  margin: "20px 0",
+}));
 
 const Home = () => {
   return (
-    <div className="home">
-      Home
-      {/* <Sidebar />
-      <div className="homeContainer">
-        <Navbar />
-        <div className="widgets">
+    <Box>
+      <Grid container spacing={2}>
+        <Grid item md={4}>
           <Widget type="user" />
+        </Grid>
+        <Grid item md={4}>
           <Widget type="order" />
+        </Grid>
+        <Grid item md={4}>
           <Widget type="earning" />
-        </div>
-        <div className="charts">
-          <Featured />
-          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
-        </div>
-        <div className="listContainer">
-          <div className="listTitle">Latest Transactions</div>
-          <TableList />
-        </div>
-      </div> */}
-    </div>
+        </Grid>
+      </Grid>
+      <Box sx={{ marginTop: "30px" }}>
+        <Grid container spacing={2}>
+          <Grid item md={4}>
+            <Featured />
+          </Grid>
+          <Grid item md={8}>
+            <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+          </Grid>
+        </Grid>
+      </Box>
+      <Box>
+        <Typographys variant="h4" sx={{}}>
+          Latest Transactions
+        </Typographys>
+        <TableList />
+      </Box>
+    </Box>
   );
 };
 
