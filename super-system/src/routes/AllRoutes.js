8eaf70aadfee5290eb7e440c5493f993d03cbe1c
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-10-15 21:12:57 +0600

    Products pages updated

--- super-system/src/routes/AllRoutes.js
+++ super-system/src/routes/AllRoutes.js
@@ -4,6 +4,8 @@ import DashboardLayout from "../layout/DashboardLayout/index";
 import Home from "../pages/Home/Home";
 import Users from "../pages/Users/Users";
 import Single from "../pages/Single/Single";
+import Products from "../pages/Products/Products";
+import Orders from "../pages/Orders/Orders";
 
 const AllRoutes = [
   {
@@ -30,6 +32,26 @@ const AllRoutes = [
       },
       {
         path: "/products",
+        element: <Products />,
+      },
+      {
+        path: "/orders",
+        element: <Orders />,
+      },
+      {
+        path: "/notifications",
+        element: <Single />,
+      },
+      {
+        path: "/logs",
+        element: <Single />,
+      },
+      {
+        path: "/profile",
+        element: <Single />,
+      },
+      {
+        path: "/settings",
         element: <Single />,
       },
     ],
